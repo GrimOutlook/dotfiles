d770cb4e700a94fdbbe98954620be267c7d6ab6c
Dominic Grimaldi <dominic.j.grimaldi@gmail.com>
2019-06-12 15:13:00 +0000

    fixed filename formatting and update installer

--- install.sh
+++ install.sh
@@ -5,15 +5,15 @@ echo -e "\n"
 
 [ "$answer" != "y" -a "$answer" != "Y" ] && echo -e "Canceling..." && exit 1
 
-CONFIG_PATH="/etc/config"
+CONFIG_PATH="~/.dotfiles"
 
-for file in "bashrc" "bash_aliases" "bash_profile" "profile" "vimrc"; do
-	if [ -h "$HOME/.$file" ]; then
-		! unlink "$HOME/.$file" && echo "Afailure has occured unlinking $file. Exiting..." && exit 1
-	elif [ -f  "$HOME/.$file" ]; then
-		! mv "$HOME/.$file" "$HOME/.${file}.old" && echo "A failure has occured moving $file. Exiting..." && exit 1
+for file in ".bashrc" ".bash_aliases" ".bash_profile" ".profile" ".vimrc" ".tmux.conf"; do
+	if [ -h "$HOME/$file" ]; then
+		! unlink "$HOME/$file" && echo "Afailure has occured unlinking $file. Exiting..." && exit 1
+	elif [ -f  "$HOME/$file" ]; then
+		! mv "$HOME/$file" "$HOME/${file}.old" && echo "A failure has occured moving $file. Exiting..." && exit 1
 	fi
-	! ln -s "$CONFIG_PATH/home/$file" "$HOME/.$file" && echo "A failure has occured linking $file. Exiting..." && exit 1
+	! ln -s "$CONFIG_PATH/$file" "$HOME/$file" && echo "A failure has occured linking $file. Exiting..." && exit 1
 done
 
 read -n 1 -p "Do you want to in install vim-plug? If it's already present this is unnecessary. (y,N)" answer
@@ -25,14 +25,5 @@ if [ $(id -u) -ne 0 ]; then
 	# sudo -i
 	# [ $(id -u) -ne 0 ] && "Still not root. Exiting..." && exit 1
 fi
-
-TMUX_CONF="/etc/tmux.conf"
-
-if [ -h "$TMUX_CONF" ]; then
-	! sudo unlink "$TMUX_CONF" && echo "A failure occured unlinking $TMUX_CONF. Exiting..." && exit 1
-elif [ -f  "$TMUX_CONF" ]; then
-	! sudo mv "$TMUX_CONF" "${TMUX_CONF}.old" && echo "A failure occured moving $TMUX_CONF. Exiting..." && exit 1
-fi
-sudo ln -s $CONFIG_PATH/etc/tmux.conf /etc/tmux.conf
 
 echo "Finished. Exiting..." && exit 0
