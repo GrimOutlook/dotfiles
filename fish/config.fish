72cf6dd099b900b458dec9395365f5024e8f3448
Dominic Grimaldi <dominic.j.grimaldi@gmail.com>
2025-04-09 14:03:32 -0500

    Exported variables needed when editing systemd services

--- fish/config.fish
+++ fish/config.fish
@@ -4,6 +4,10 @@ set fish_function_path (path resolve $__fish_config_dir/functions/*/) $fish_func
 # Add completion subdirs to fish_completion_path
 set fish_complete_path (path resolve $__fish_config_dir/completions/*/) $fish_complete_path
 
+# Make nvim the default editor
+set -gx EDITOR "/usr/bin/nvim"
+set -gx SYSTEMD_EDITOR "/usr/bin/nvim"
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
