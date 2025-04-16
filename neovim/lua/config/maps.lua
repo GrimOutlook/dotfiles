6369c11c3a0dfd874323cf5610656c2f48044263
Dominic Grimaldi <dominic.j.grimaldi@gmail.com>
2025-04-16 15:10:01 -0500

    Remove neotree maps as they are included with snacks

--- neovim/lua/config/maps.lua
+++ neovim/lua/config/maps.lua
@@ -4,10 +4,10 @@ local function map_call(mode, lhs, rhs, silent)
 	vim.keymap.set(mode, lhs, rhs, { silent = silent })
 end
 local function maps(mode, lhs, rhs)
-	map_call(mode, lhs, rhs, true )
+	map_call(mode, lhs, rhs, true)
 end
 local function map(mode, lhs, rhs)
-	map_call(mode, lhs, rhs, false )
+	map_call(mode, lhs, rhs, false)
 end
 
 -- Save
@@ -18,10 +18,6 @@ maps("n", "<leader>q", "<CMD>q<CR>")
 
 -- Exit insert mode
 maps("i", "jk", "<ESC>")
-
--- NeoTree
-maps("n", "<leader>ee", "<CMD>Neotree toggle<CR>")
-maps("n", "<leader>er", "<CMD>Neotree focus<CR>")
 
 -- Window Navigation
 maps("n", "<C-h>", "<C-w>h")
@@ -43,5 +39,6 @@ maps("n", "<C-Down>", "<C-w>-")
 maps("n", "<leader>r", vim.lsp.buf.rename)
 
 -- Snacks
-maps("n", "<leader>j", function() Snacks.picker.jumps() end)
-
+maps("n", "<leader>j", function()
+	Snacks.picker.jumps()
+end)
