cf00cfa29a26416203031a6e5c7ff3e6f7745315
Dominic Grimaldi <dominic.j.grimaldi@gmail.com>
2025-04-19 07:06:16 -0500

    Added missed files from previous commits

---
+++ bash/scripts/mklink.bash
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+function mklink() {
+    if [ ! $# -eq 2 ]; then
+        echo "Only 2 arguments allowed when calling mklink" >&2
+        return 1
+    fi
+
+    source=$1
+
+    destination=$2
+    if [ -e "$destination" ]; then
+        bkp "$destination" || return 1
+    fi
+
+    destination_directory="$(dirname "$destination")"
+    if [ ! -d "$destination_directory" ]; then
+        mkdir -p "$destination_directory" || {
+            echo "Failed to create directory"
+            return 1
+        }
+    fi
+
+    ln -s "$source" "$destination" || {
+        echo "Failed to link $source to $destination" >&2
+        return 1
+    }
+}
