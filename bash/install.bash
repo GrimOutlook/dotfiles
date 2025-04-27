e7cdb74e6c9c3d3f893af4ce3df8c5a4932cf468
Dominic Grimaldi <dominic.j.grimaldi@gmail.com>
2025-04-27 18:30:49 -0500

    Made quite a few changes to dotfiles

--- bash/install.bash
+++ bash/install.bash
@@ -5,7 +5,7 @@ BASH_DIR=$DOTFILES/bash
 
 mklink=$BASH_DIR/scripts/mklink
 
-$mklink $BASH_DIR/.bashrc $HOME/.bashrc || exit 1
-$mklink $BASH_DIR/.bash_aliases $HOME/.bash_aliases || exit 1
-$mklink $BASH_DIR/.bash_completions $HOME/.bash_completions || exit 1
+$mklink $BASH_DIR/bashrc $HOME/.bashrc || exit 1
+$mklink $BASH_DIR/bash_aliases $HOME/.bash_aliases || exit 1
+$mklink $BASH_DIR/bash_completions $HOME/.bash_completions || exit 1
 
