9e327b4b7f5c7ae08f3b62e60df52b60619bc26f
Dominic Grimaldi <dominic.j.grimaldi@gmail.com>
2025-05-15 09:23:12 -0500

    Add line wrapping back

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,3 +1,14 @@
--- Options are automatically loaded before lazy.nvim startup
+-- Options are automatically loaded before lazy.nvim start up
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+--don't call globals unnecessarily
+local o = vim.o
+local bo = vim.bo --> buffer only use bo
+local wo = vim.wo --> window only use wo
+
+o.linebreak = true -- Lines wrap at words rather than random characters
+o.number = true
+o.showbreak = [[↪ ]] -- Options include -> '…', '↳ ', '→','↪ '
+o.colorcolumn = "+1" -- Set the color column to highlight one column after the 'textwidth'
+o.textwidth = 80
+o.formatoptions = "1qcrn2tjlv"
