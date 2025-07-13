4ba999ca1e6fd08a4939af868c7974cef8767949
Dominic Grimaldi <dominic.j.grimaldi@gmail.com>
2025-07-13 13:14:56 -0500

    fix: Added QoL options to blink

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -1,5 +1,23 @@
 require('blink.cmp').setup({
-    opts = {
+    keymap = {
         preset = 'super-tab'
-    }
+    },
+    completion = {
+        accept = {
+            auto_brackets = {
+                enabled = true,
+            },
+        },
+        documentation = {
+            auto_show = true,
+            auto_show_delay_ms = 200,
+            window = { border = "rounded", },
+        },
+        ghost_text = {
+            enabled = true,
+        },
+    },
+    signature = {
+        enabled = true,
+    },
 })
