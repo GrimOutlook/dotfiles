373e70a77a7e4ffbefd128bd85294708ec52be72
Dominic Grimaldi <dominic.j.grimaldi@gmail.com>
2025-07-13 13:50:57 -0500

    feat: Add formatting plugin

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,28 +1,28 @@
 local vim = vim
-local Plug = vim.fn['plug#']
+local Plug = vim.fn["plug#"]
 vim.g.mapleader = " "
 
-vim.call('plug#begin')
+vim.call("plug#begin")
 
 -- Themes
 Plug("ellisonleao/gruvbox.nvim")
 
 -- Icons
 -- Icons for dashboard and fzf-lua
-Plug('nvim-tree/nvim-web-devicons')
+Plug("nvim-tree/nvim-web-devicons")
 -- Icons for ranger
-Plug 'echasnovski/mini.files'
+Plug("echasnovski/mini.files")
 -- Icons for which-key
-Plug 'echasnovski/mini.icons'
+Plug("echasnovski/mini.icons")
 
 -- Dashboard
-Plug('goolord/alpha-nvim')
+Plug("goolord/alpha-nvim")
 
 -- Keymaps
 Plug("folke/which-key.nvim")
 
 -- File Manager
-Plug('kevinhwang91/rnvimr')
+Plug("kevinhwang91/rnvimr")
 
 -- LSP
 Plug("mason-org/mason.nvim")
@@ -30,42 +30,45 @@ Plug("mason-org/mason-lspconfig.nvim")
 Plug("neovim/nvim-lspconfig")
 
 -- Snippets
-Plug('rafamadriz/friendly-snippets')
+Plug("rafamadriz/friendly-snippets")
+Plug("L3MON4D3/LuaSnip", { ["tag"] = "v2.*", ["do"] = "make install_jsregexp" })
 
 -- CMP
-Plug("saghen/blink.cmp", {["version"] = '1.*'})
+Plug("saghen/blink.cmp", { ["version"] = "1.*" })
 
-Plug('L3MON4D3/LuaSnip', {['tag']= 'v2.*', ['do']= 'make install_jsregexp'} )
+-- Formatting
+Plug("stevearc/conform.nvim")
 
 -- Terminal
-Plug('akinsho/toggleterm.nvim', {['tag'] = '*'})
+Plug("akinsho/toggleterm.nvim", { ["tag"] = "*" })
 
 -- Pickers/Finders
-Plug("nvim-telescope/telescope.nvim", { ['tag'] = '0.1.8' } )
+Plug("nvim-telescope/telescope.nvim", { ["tag"] = "0.1.8" })
 -- Dependency for above
 Plug("nvim-lua/plenary.nvim")
 -- Project Picker
-Plug('ahmedkhalf/project.nvim')
+Plug("ahmedkhalf/project.nvim")
 
 -- Task tracking
-Plug('Hashino/doing.nvim')
+Plug("Hashino/doing.nvim")
 
 -- Statusline
-Plug('nvim-lualine/lualine.nvim')
+Plug("nvim-lualine/lualine.nvim")
 
-vim.call('plug#end')
+vim.call("plug#end")
 
-require "user.keymaps"
-require "user.autocmds"
-require "user.options"
+require("user.keymaps")
+require("user.autocmds")
+require("user.options")
 
-require "plugins.alpha"
-require "plugins.blink"
-require "plugins.lualine"
-require "plugins.mason" -- This must come before mason-lspconfig
-require "plugins.mason-lspconfig"
-require "plugins.projects"
-require "plugins.toggleterm"
-require "plugins.which-key"
+require("plugins.alpha")
+require("plugins.blink")
+require("plugins.conform")
+require("plugins.lualine")
+require("plugins.mason") -- This must come before mason-lspconfig
+require("plugins.mason-lspconfig")
+require("plugins.projects")
+require("plugins.toggleterm")
+require("plugins.which-key")
 
-vim.cmd('colorscheme gruvbox')
+vim.cmd("colorscheme gruvbox")
