3d3997322991f248135bf0dbdeeabdf76f21070d
Dominic Grimaldi <dominic.j.grimaldi@gmail.com>
2025-08-18 08:32:31 -0500

    feat: Added git to dashboard

--- nvim/lua/plugins/snacks/dashboard.lua
+++ nvim/lua/plugins/snacks/dashboard.lua
@@ -1,25 +1,26 @@
 return {
-  "folke/snacks.nvim",
-  opts = {
-    dashboard = {
-      preset = {
-        pick = function(cmd, opts)
-          return LazyVim.pick(cmd, opts)()
-        end,
-        -- stylua: ignore
-        ---@type snacks.dashboard.Item[]
-        keys = {
-          { icon = " ", key = "p", desc = "Projects", action = ":lua Snacks.dashboard.pick('projects')" },
-          { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
-          { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
-          { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
-          { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
-          { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
-          { icon = " ", key = "s", desc = "Restore Session", action = ":lua require('persistence').load({ last = true })" },
-          { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy" },
-          { icon = " ", key = "q", desc = "Quit", action = ":qa" },
-        },
-        header = [[
+    "folke/snacks.nvim",
+    opts = {
+        dashboard = {
+            preset = {
+                pick = function(cmd, opts)
+                    return LazyVim.pick(cmd, opts)()
+                end,
+                -- stylua: ignore
+                ---@type snacks.dashboard.Item[]
+                keys = {
+                    { icon = " ", key = "p", desc = "Projects", action = ":lua Snacks.dashboard.pick('projects')" },
+                    { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+                    { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+                    { icon = " ", key = "/", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+                    { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+                    { icon = " ", key = "g", desc = "Gitui", action = ":lua Snacks.terminal({ 'gitui' })" },
+                    { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
+                    { icon = " ", key = "s", desc = "Restore Session", action = ":lua require('persistence').load({ last = true })" },
+                    { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy" },
+                    { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+                },
+                header = [[
             )  (    (      (    (      (     (        )
   *   )  ( /(  )\ ) )\ )   )\ ) )\ )   )\ )  )\ )  ( /(
 ` )  /(  )\())(()/((()/(  (()/((()/(  (()/( (()/(  )\()) (
@@ -29,21 +30,21 @@ return {
   | |   | __ | | | \__ \   | | \__ \  | __|  | |  | .` || _|
   |_|   |_||_||___||___/  |___||___/  |_|   |___| |_|\_||___|
 ]],
-      },
-      sections = {
-        {
-          section = "terminal",
-          cmd = "/usr/bin/cat ~/.config/nvim/resources/wall.output; sleep 0.1",
-          padding = 1,
-          height = 30,
+            },
+            sections = {
+                {
+                    section = "terminal",
+                    cmd = "/usr/bin/cat ~/.config/nvim/resources/wall.output; sleep 0.1",
+                    padding = 1,
+                    height = 30,
+                },
+                {
+                    pane = 2,
+                    { section = "header" },
+                    { section = "keys",   gap = 1, padding = 1 },
+                    { section = "startup" },
+                },
+            },
         },
-        {
-          pane = 2,
-          { section = "header" },
-          { section = "keys", gap = 1, padding = 1 },
-          { section = "startup" },
-        },
-      },
     },
-  },
 }
