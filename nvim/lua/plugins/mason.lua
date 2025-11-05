2ccf3b4011855ed6681ea7d50c26f528c5438483
Dominic Grimaldi <dominic.j.grimaldi@gmail.com>
2025-11-05 07:48:14 -0600

    wip!

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -1,105 +1,127 @@
 local lsp_install = {
-    "codebook",
-    "typos_lsp",
-    "bashls",
-    "lua_ls",
-    "jdtls",
-    "gh_actions_ls",
-    "ast_grep",
+  -- Spelling
+  "codebook",
+  "typos_lsp",
+  -- Bash
+  "bashls",
+  -- Lua
+  "lua_ls",
+  -- Java
+  "jdtls",
+  -- Github Action Files
+  "gh_actions_ls",
+  -- General
+  "ast_grep",
+  -- Docker
+  -- NOTE: Could also try the `docker_compose_language_service` but it doesn't support `Dockerfile`s. It's made by Microsoft while `docker_language_server` is made by docker.
+  "docker_language_server",
+  -- TSX/HTML/Markup
+  "emmet_language_server",
+  -- Typescript/Javascript
+  -- NOTE: `oxlint` aims to replace `eslint`. Swap for `eslint` if shortcomings are found.
+  -- NOTE: `tsgo` is way more popular (Github stars). Might checkout `ts_ls` later if issues are found.
+  -- "ts_ls",
+  -- "eslint",
+  -- "tsgo",
+  "oxlint",
+  -- Gitlab CI Files
+  "gitlab_ci_ls",
+  -- Gradle Files
+  "gradle_ls",
+  -- Groovy
+  "groovyls",
+  -- Hypr Config Files
+  "hyprls",
+  -- JSON
+  "lemminx",
+  "jqls",
+  -- JUSTFILEs
+  "just",
+  -- XML
+  -- Markdown
+  "markdown_oxide",
+  -- Powershell
+  "powershell_es",
+  -- RPM SPEC files
+  "rpmspec",
+  -- Code security scanner. Might use remote calls, disabling for now.
+  -- "snyk_ls",
+  --
+  -- CSS/Vue/HTML
+  "stylelint_lsp",
+  -- Systemd Unit Files
+  "systemd_ls",
+  -- CSS
+  "tailwindcss",
 
-    -- TODO: Determine if the below might be better
-    -- "docker_compose_language_service",
-    -- "dockerls",
-    "docker_language_server",
+  -- TOML
+  -- NOTE: `tombi` is newer and is currently more geared towards cargo toml
+  -- files but it is a potential successor to `taplo`. Can switch if the need
+  -- arises.
+  -- "taplo",
+  "tombi",
 
-    "emmet_language_server",
-    "eslint",
-    "gitlab_ci_ls",
-    "gradle_ls",
-    "groovyls",
-    "hyprls",
-    "jqls",
-    "just",
-    "lemminx",
-    "markdown_oxide",
-    "oxlint",
-    "powershell_es",
-    "rpmspec",
-    "snyk_ls",
-    "stylelint_lsp",
-    "systemd_ls",
-    "tailwindcss",
-
-    -- TODO: Figure out if the below might be better
-    -- "taplo",
-    "tombi",
-
-    -- TODO: Figure out if the below might be better
-    -- "tsgo",
-    "ts_ls",
-
-    "yamlls",
+  "yamlls",
 }
 local tools_install = {
-    "actionlint",
-    "black",
-    "cbfmt",
-    "codelldb",
-    "commitlint",
-    "cpptools",
-    "doctoc",
-    "dotenv-linter",
-    "editorconfig-checker",
-    "eslint-lsp",
-    "fixjson",
-    "gitleaks",
-    "gitlint",
-    "gitui",
-    "glow",
-    "google-java-format",
-    "isort",
-    "mdslw",
-    "misspell",
-    "pgformatter",
-    "prettier",
-    "prettierd",
-    "rpmlint",
-    "rustywind",
-    "semgrep",
-    "shellcheck",
-    "shellharden",
-    "shfmt",
-    "vscode-java-decompiler",
-    "vscode-java-dependency",
+  "actionlint",
+  "black",
+  "cbfmt",
+  "codelldb",
+  "commitlint",
+  "cpptools",
+  "doctoc",
+  "dotenv-linter",
+  "editorconfig-checker",
+  "fixjson",
+  "gitleaks",
+  "gitlint",
+  "gitui",
+  "glow",
+  "google-java-format",
+  "isort",
+  "mdslw",
+  "misspell",
+  "pgformatter",
+  "prettier",
+  "prettierd",
+  "rpmlint",
+  "rustywind",
+  "semgrep",
+  "shellcheck",
+  "shellharden",
+  "shfmt",
+  "vscode-java-decompiler",
+  "vscode-java-dependency",
 }
 local dependencies = {
-    "mason-org/mason-registry",
-    {
-        "mason-org/mason-lspconfig.nvim",
-        dependencies = {
-            {
-                "mason-org/mason.nvim",
-                version = "^2.0.0",
-                opts = {},
-            },
-        },
+  "mason-org/mason-registry",
+  {
+    "mason-org/mason-lspconfig.nvim",
+    dependencies = {
+      {
+        "mason-org/mason.nvim",
         version = "^2.0.0",
-        opts = {
-            ensure_installed = lsp_install,
-        },
+        opts = {},
+      },
     },
+    version = "^2.0.0",
+    opts = {
+      ensure_installed = lsp_install,
+    },
+  },
 }
 
 return {
-    -- Used to define tools in configs so you don't have to manually install
-    -- them from the Mason menu.
-    -- NOTE: mason-lspconfig only works for LSP tools. This works for all tool
-    -- catgories.
-    -- NOTE: mason-tool-install.nvim can accept either Mason names, lspconfig
-    -- names or dap names.
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-    dependencies = dependencies,
-    opts = {
-        ensure_installed = tools_install,
-    },
+  -- Used to define tools in configs so you don't have to manually install
+  -- them from the Mason menu.
+  -- NOTE: mason-lspconfig only works for LSP tools. This works for all tool
+  -- categories.
+  -- NOTE: mason-tool-install.nvim can accept either Mason names, lspconfig
+  -- names or dap names.
+  "WhoIsSethDaniel/mason-tool-installer.nvim",
+  dependencies = dependencies,
+  opts = {
+    ensure_installed = tools_install,
+  },
 }
