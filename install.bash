f915175945061c5fc8274a0ea3a017e098c3ecdc
Dominic Grimaldi <self@grimoutlook.dev>
2025-11-25 20:42:34 -0600

    wip!

--- install.bash
+++ install.bash
@@ -5,17 +5,17 @@ export SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd
 export PATH+=":$SCRIPT_DIR/bash/scripts/"
 
 export DOTFILES="$SCRIPT_DIR"
-export CONFIG="$HOME/config/"
+export CONFIG="$HOME/.config/"
 
 echo "Dotfiles folder: $DOTFILES"
 
 # TODO: Use the install_files variable from below to check what doesn't have an
 # install file.
 fd -t d --max-depth 1 --absolute-path -x \
-    bash -c 'test -f "{}/install.bash" || echo "{} does not have an install script." >&2 ' \; . "$DOTFILES"
+  bash -c 'test -f "{}/install.bash" || echo "{} does not have an install script." >&2 ' \; . "$DOTFILES"
 
 install_files=$(fd -t f --absolute-path --min-depth 2 "install.bash" "$DOTFILES")
 for install in $install_files; do
-    chmod +x "$install"
-    "$install"
+  chmod +x "$install"
+  "$install"
 done
