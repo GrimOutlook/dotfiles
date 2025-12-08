de8b2e180eb14c396dfed6da1c1fa7b466c13b25
Dominic Grimaldi <dominic.j.grimaldi@gmail.com>
2025-12-08 14:48:28 -0600

    wip!

--- nvim/lua/plugins/nvim-dap-ui.lua
+++ nvim/lua/plugins/nvim-dap-ui.lua
@@ -11,15 +11,19 @@ return {
     local dap = require("dap")
     local dapui = require("dapui")
     dapui.setup(opts)
-    dap.listeners.after.event_initialized["dapui_config"] = function()
-      dapui.open({})
-    end
-    dap.listeners.before.event_terminated["dapui_config"] = function()
-      dapui.close({})
-    end
-    dap.listeners.before.event_exited["dapui_config"] = function()
-      dapui.close({})
-    end
+    -- WARN: This is causing DAP to close when invalid args are given, making it
+    -- harder to determine what args are wrong when trying to run the command
+    -- again
+    --
+    -- dap.listeners.after.event_initialized["dapui_config"] = function()
+    --   dapui.open({})
+    -- end
+    -- dap.listeners.before.event_terminated["dapui_config"] = function()
+    --   dapui.close({})
+    -- end
+    -- dap.listeners.before.event_exited["dapui_config"] = function()
+    --   dapui.close({})
+    -- end
     require("lazydev").setup({
       library = { "nvim-dap-ui" },
     })
